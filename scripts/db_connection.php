712c4f6bd8ecad16cb82ea85f0f902d13763bc6e
Shanre Scheepers <21100387@virtualwindow.co.za>
2022-05-22 17:36:07 +0200

    php added from draft

---
+++ scripts/db_connection.php
@@ -0,0 +1,11 @@
+<?php
+
+$dbhost = "localhost";
+$dbuser = "root";
+$dbpassword = "root";
+// edited Mike's DB name to task2_crud_exercise as my DB for Task Two
+$db = "task2_crud_exercise";
+
+$conn = new mysqli($dbhost, $dbuser, $dbpassword, $db);
+
+?>
